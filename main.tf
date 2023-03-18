dfd69be60783c5867d82aaefe21be08d06c78719
Char1410 <109825853+Char1410@users.noreply.github.com>
2023-03-18 18:37:03 -0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,36 @@
+# Configure the AWS Provider
+provider "aws" {
+  region     = "us-east-2"
+  access_key = "AKIAYQA3L4DEUSC2CJTC"
+  secret_key = "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"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  key_name = "terraform"
+
+  tags = {
+    Name = "web-server"
+  }
+}
+
+output "public_ip" {
+  value       = aws_instance.web.public_ip
+}
