e20dddc270e58c6d7923095378097a4eef20cbb1
Char1410 <109825853+Char1410@users.noreply.github.com>
2023-03-19 23:01:45 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Configure the AWS Provider
 provider "aws" {
-  region     = "us-east-2"
+  region     = "us-east-1"
   access_key = "AKIAYQA3L4DEUSC2CJTC"
   secret_key = "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"
 }
