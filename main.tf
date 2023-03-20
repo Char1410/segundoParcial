9910f74b17b2035453cfdd005879744b96bffcf2
Char1410 <109825853+Char1410@users.noreply.github.com>
2023-03-19 23:00:57 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,7 +24,6 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  key_name = "terraform"
 
   tags = {
     Name = "web-server"
